e49620f8d1fa8887c7964a00f51e5b2dfb9f4500
dloyolar <dieegolr@gmail.com>
2021-08-14 17:32:50 -0400

    refactor: use helpers and hooks to fetch data

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,33 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async () => {
-    const url =
-      'https://api.giphy.com/v1/gifs/search?q=one+piece&limit=25&api_key=YzfEcHSohd23s7SuJ3HGLmsMEzUGtXY0';
-    const res = await fetch(url);
-    const { data } = await res.json();
-
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-
-    setImages(gifs);
-  };
+  const { data: images, loading } = useFetchGifs(category);
 
   return (
     <>
       <h3> {category} </h3>
+      {loading && <p>Loading</p>}
       <div className='card-grid'>
         {images.map((image) => (
           <GifGridItem key={image.id} {...image} />
