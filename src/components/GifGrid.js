ac720d69313849ec1e4acb60f42609acdb9a899a
dloyolar <dieegolr@gmail.com>
2021-08-14 16:54:50 -0400

    feat: add gif item component with styles

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-  const [count, setCount] = useState(0);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     getGifs();
@@ -21,14 +22,17 @@ export const GifGrid = ({ category }) => {
       };
     });
 
-    console.log(gifs);
+    setImages(gifs);
   };
 
   return (
     <>
       <h3> {category} </h3>
-      <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)}></button>
+      <div className='card-grid'>
+        {images.map((image) => (
+          <GifGridItem key={image.id} {...image} />
+        ))}
+      </div>
     </>
   );
 };
