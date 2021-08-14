0ca99a605846ce734688787aff318d559241c09d
dloyolar <dieegolr@gmail.com>
2021-08-14 15:52:04 -0400

    feat: add new component to send input value to parent

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifApp = () => {
+  const [categories, setCategories] = useState([
+    'One Punch',
+    'Kimetsu no Yaiba',
+    'Nagatoro',
+  ]);
+
   return (
     <>
       <h2>GifApp</h2>
+      <AddCategory setCategories={setCategories} />
       <hr />
+
+      <ol>
+        {categories.map((catergory) => {
+          return <li key={catergory}>{catergory}</li>;
+        })}
+      </ol>
     </>
   );
 };
